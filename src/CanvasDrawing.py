1ca4c851f85c70416f7b08701be981935b23f3b4
GitMew <tomi200@telenet.be>
2020-09-14 02:21:55 +0200

    Converted canvas drawing functions to Grid class

--- src/CanvasDrawing.py
+++ src/CanvasDrawing.py
@@ -27,33 +27,40 @@ GRID_Y = 10
 SCREEN_WIDTH  = 95
 SCREEN_HEIGHT = 63
 
+# FUNCTIONS AND CLASSES
+# Grid layer class (because almost all drawing functions need arguments (display, colour, grid_x, grid_y); actually, they can just be methods for a grid, now that I think about it)
+class Grid:
+    def __init__(self, layer_colour: tuple=COL_TI_BLACK, gridcell_px_x: int=GRID_X, gridcell_px_y: int=GRID_Y,
+                 cellcount_x: int=SCREEN_WIDTH, cellcount_y: int=SCREEN_HEIGHT):
+        pygame.init()
+        self.dis = pygame.display.set_mode((GRID_X * SCREEN_WIDTH, GRID_Y * SCREEN_HEIGHT))
+        pygame.display.set_caption("TI-84+ Emulator")
 
-# Hierarchy of drawing functions
-def coordToCell(x: int, y: int,
-                dis, colour, grid_x: int=GRID_X, grid_y: int=GRID_Y):
-    pygame.draw.rect(dis, colour, [x*grid_x, y*grid_y, grid_x, grid_y])
+        self.colour = layer_colour
 
+        self.cellcount_x = cellcount_x
+        self.cellcount_y = cellcount_y
 
-def matrixToCells(matrix: list, topleft_x: int, topleft_y: int,
-                  dis, colour, grid_x: int=GRID_X, grid_y: int=GRID_Y):
-    for y in range(len(matrix)):
-        for x in range(len(matrix[0])):
-            if matrix[y][x]:
-                coordToCell(topleft_x + x, topleft_y + y, dis, colour, grid_x, grid_y)
+        self.grid_x = gridcell_px_x
+        self.grid_y = gridcell_px_y
 
+    # Hierarchy of drawing functions
+    def coordToCell(self, x: int, y: int):
+        pygame.draw.rect(self.dis, self.colour, [x*self.grid_x, y*self.grid_y, self.grid_x, self.grid_y])
 
-def charToCells(character: str, lower: bool, topleft_x: int, topleft_y: int,
-                dis, colour, grid_x: int=GRID_X, grid_y: int=GRID_Y):
-    matrixToCells(
-        (UPPER_MATRICES if not lower else LOWER_MATRICES).get(character, [[]]), topleft_x, topleft_y,
-        dis, colour, grid_x, grid_y
-    )
+    def matrixToCells(self, matrix: list, topleft_x: int, topleft_y: int):
+        for y in range(len(matrix)):
+            for x in range(len(matrix[0])):
+                if matrix[y][x]:
+                    self.coordToCell(topleft_x + x, topleft_y + y)
 
+    def charToCells(self, character: str, lower: bool, topleft_x: int, topleft_y: int):
+        self.matrixToCells(
+            (UPPER_MATRICES if not lower else LOWER_MATRICES).get(character, [[]]), topleft_x, topleft_y
+        )
 
-def textToCells(text: str, lower: bool, topleft_x: int, topleft_y: int,
-                dis, colour, grid_x: int=GRID_X, grid_y: int=GRID_Y):
-    offset = 0
-    for c in text:
-        charToCells(c, lower, topleft_x + offset, topleft_y,
-                    dis, colour, grid_x, grid_y)
-        offset += len((UPPER_MATRICES if not lower else LOWER_MATRICES).get(c,[[]])[0]) + 1
+    def textToCells(self, text: str, lower: bool, topleft_x: int, topleft_y: int):
+        offset = 0
+        for c in text:
+            self.charToCells(c, lower, topleft_x + offset, topleft_y)
+            offset += characterWidth(c, lower) + 1
